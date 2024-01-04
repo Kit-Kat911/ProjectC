f1a170aa6a011733aeeed711df953d94b92e3859
kayla <kayla@LAPTOP-QJ90T6UQ.mynetworksettings.com>
2024-01-03 21:02:08 -0500

    trying the things

--- src/c/DemoC.java
+++ src/c/DemoC.java
@@ -5,6 +5,8 @@ public class DemoC {
 	public static void main(String[] args) {
 		System.out.println("CCCCCCCCCC");
 		System.out.println("CCCCCCCCCC");
+		System.out.println("CCCCCCCCCC");
+		//https://github.com/Kit-Kat911/ProjectC.git
 	}
 
 }
